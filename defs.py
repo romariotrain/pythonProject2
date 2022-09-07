e9b2eeaaaf31b609bbc561d71076d4c80aa4930e
Морозевич Роман <romamorozevich@yandex.ru>
2022-09-07 17:01:12 +0300

    second commit

--- defs.py
+++ defs.py
@@ -1,42 +1,56 @@
+import re
+
+
 def line_names(contacts_list):
-  for contact in contacts_list:
-    contact[0] = contact[0] + ' ' + contact[1] + ' ' + contact[2]
-    contact[1], contact[2] = '', ''
-    contact[0] = contact[0].split()
-    if len(contact[0]) == 3:
-      contact[1] = contact[0][1]
-      contact[2] = contact[0][2]
-      contact[0] = contact[0][0]
-    else:
-      contact[1] = contact[0][1]
-      contact[0] = contact[0][0]
+    for contact in contacts_list:
+        contact[0] = contact[0] + ' ' + contact[1] + ' ' + contact[2]
+        contact[1], contact[2] = '', ''
+        contact[0] = contact[0].split()
+        if len(contact[0]) == 3:
+            contact[1] = contact[0][1]
+            contact[2] = contact[0][2]
+            contact[0] = contact[0][0]
+        else:
+            contact[1] = contact[0][1]
+            contact[0] = contact[0][0]
+        if len(contact) > 7:
+            contact.pop(-1)
 
 
 def check_repeatable_names(contacts_list, repeatable_contacts):
-  for contact in contacts_list:
-    for suspect in contacts_list:
-      if contact[0] == suspect[0] and contact[1] == suspect[1] and contact != suspect:
-        repeatable_contacts.append(contact)
+    for contact in contacts_list:
+        for suspect in contacts_list:
+            if contact[0] == suspect[0] and contact[1] == suspect[1] and contact != suspect:
+                repeatable_contacts.append(contact)
 
 
 def add_data(repeatable_contacts):
-  for repeatable_person in repeatable_contacts:
-    base = -1
-    for info in repeatable_person:
-      base += 1
-      if info == '':
-        for repeatable_person_ in repeatable_contacts:
-          if repeatable_person_[0] == repeatable_person[0] and repeatable_person_[1] == repeatable_person[1] and repeatable_person != repeatable_person_ :
-            repeatable_person[base] = repeatable_person_[base]
+    for repeatable_person in repeatable_contacts:
+        base = -1
+        for info in repeatable_person:
+            base += 1
+            if info == '':
+                for repeatable_person_ in repeatable_contacts:
+                    if repeatable_person_[0] == repeatable_person[0] and repeatable_person_[1] == repeatable_person[
+                        1] and repeatable_person != repeatable_person_:
+                        repeatable_person[base] = repeatable_person_[base]
 
 
 def change_names(contacts_list, repeatable_contacts):
-  for contact in contacts_list:
-    for repeatable_person in repeatable_contacts:
-      if contact[0] == repeatable_person[0] and contact[1] == repeatable_person[1] and contact != repeatable_person:
-        contact = repeatable_person
+    for contact in contacts_list:
+        for repeatable_person in repeatable_contacts:
+            if contact[0] == repeatable_person[0] and contact[1] == repeatable_person[
+                1] and contact != repeatable_person:
+                contact = repeatable_person
 
-def create_new_list(contacts_list,new_contacts_list):
-  for contact in contacts_list:
-    if contact not in new_contacts_list:
-      new_contacts_list.append(contact)
+
+def create_new_list(contacts_list, new_contacts_list):
+    for contact in contacts_list:
+        if contact not in new_contacts_list:
+            new_contacts_list.append(contact)
+
+
+def change_phone(pattern1, substitution1, new_contacts_list):
+    for phone in new_contacts_list:
+        changed_phone = re.sub(pattern1, substitution1, phone[-2])
+        phone[-2] = changed_phone.strip()
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import re
 import csv
-from defs import line_names, check_repeatable_names, add_data, change_names, create_new_list
+from defs import line_names, check_repeatable_names, add_data, change_names, create_new_list, change_phone
+from pprint import pprint
 
 repeatable_contacts = []
 base = 0
 new_contacts_list = []
 contacts = ''
-pattern1 = r"(\+7|8)[\s]?\(?(\d+)\)?[-\s]?(\d{3})[-\s]?(\d{2})[-\s]?(\d{2})[ ]\(?доб. (\d+)"
-substitution1 = r"+7(\2)\3-\4-\5 доб.\6"
+pattern1 = r"(\+7|8)[\s]?\(?(\d+)\)?[-\s]?(\d{3})[-\s]?(\d{2})[-\s]?(\d{2})[ ]?\(?(доб.)?[ ]?(\d+)?(\))?"
+substitution1 = r'+7(\2)\3-\4-\5 \6\7'
 result = []
 
 if __name__ == '__main__':
-    with open("phonebook_raw.csv", encoding='utf8') as f:
+    with open("phonebook_raw.csv", encoding='cp1251') as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
 
@@ -20,10 +21,10 @@ if __name__ == '__main__':
     add_data(repeatable_contacts)
     change_names(contacts_list, repeatable_contacts)
     create_new_list(contacts_list, new_contacts_list)
-    for new_contact in new_contacts_list:
-        changed_phone = re.sub(pattern1, substitution1, new_contact[-2])
-        new_contact[-2] = changed_phone.strip()
+    change_phone(pattern1, substitution1, new_contacts_list)
 
-    with open("phonebook.csv", "w", encoding='1251') as f:
+
+
+    with open("phonebook.csv", "w", encoding='cp1251') as f:
         datawriter = csv.writer(f, delimiter=',')
         datawriter.writerows(new_contacts_list)
